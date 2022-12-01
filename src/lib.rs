7694b8b11182fb47138caf409eaeeacaa0bae25f
赵光建 <zhaoguangjian@cyberkl.com>
2022-12-01 11:57:28 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use std::fs::File;
+
+pub fn read_file_by_line() {
+    let file = File::open("./data/nginx-access_107.log-20190101").unwrap();
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use lib::read_file_by_line;
+
+mod lib;
+fn main() {
+    read_file_by_line();
+}
